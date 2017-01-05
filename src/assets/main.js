a88c1b275cd115e367886f110b426a1c0399f92c
Clive Harber <clive@whitemonkeysoftware.co.uk>
2017-01-05 17:50:06 +0000

    Project commit after all tasks

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,23 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/whitemonkey.json',
+    dataType: 'jsonp',
+    success: function (response) {
+      var courses = [];
+      courses = response.courses.completed.map(function (item) {
+        return $('<div/>')
+                .addClass('course')
+                .append($('<h3/>').text(item.title))
+                .append($('<img/>').attr({src: item.badge}))
+                .append($('<a/>').attr({
+                  href: item.url,
+                  target: '_blank'
+                }).addClass('btn btn-primary').text('See Course'));
+      });
+
+      $('#badges').append(courses);
+    }
+  });
 
 });
